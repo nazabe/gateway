57fb124a2d0e9b2a8bfc8d4f0f01269f730664f0
nazabe <nazarenobeauvais@gmail.com>
2025-04-24 17:49:17 -0300

    build(main): added get_device_uuid()

--- main/main.c
+++ main/main.c
@@ -16,33 +16,62 @@
 #include "esp_mac.h"
 #include "wifi_manager.h"
 
+#include "secrets.h"
+
+char *DEVICE_UUID = "DEFAULT";
+char *DEVICE_MAC = "TOKEN_NOT_RECIVED";
+char *DEVICE_TOKEN = "TOKEN_NOT_RECIVED";
+
 /* @brief tag used for ESP serial console messages */
 static const char TAG[] = "main";
 
-/**
- * @brief RTOS task that periodically prints the heap memory available.
- * @note Pure debug information, should not be ever started on production code! This is an example on how you can integrate your code with wifi-manager
- */
-void monitoring_task(void *pvParameter)
+bool is_wifi_connected()
 {
-	for(;;){
-		ESP_LOGI(TAG, "free heap: %"PRIu32, esp_get_free_heap_size());
-		vTaskDelay( pdMS_TO_TICKS(10000) );
-	}
+    wifi_ap_record_t ap_info;
+    if (esp_wifi_sta_get_ap_info(&ap_info) == ESP_OK)
+    {
+        return true;
+    }
+    return false;
 }
 
-/**
- * @brief this is an exemple of a callback that you can setup in your own app to get notified of wifi manager event.
- */
+int get_device_uuid(uuid, token) {
 
-void cb_connection_ok(void *pvParameter){
-	ip_event_got_ip_t* param = (ip_event_got_ip_t*)pvParameter;
+    char request_data[256];
+    snprintf(request_data, sizeof(request_data), 
+             "{\"uuid\":\"%s\",\"device_type\":\"%s\",\"ssid\":\"%s\",\"password\":\"%s\",\"token\":\"%s\"}", 
+             uuid, device_type, wifi_ssid, wifi_password, token); // TODO: Add 
 
-	/* transform IP to human readable string */
-	char str_ip[16];
-	esp_ip4addr_ntoa(&param->ip_info.ip, str_ip, IP4ADDR_STRLEN_MAX);
+    esp_http_client_config_t config = {
+        .url = API_URL,
+        .method = HTTP_METHOD_POST,
+        .crt_bundle_attach = esp_crt_bundle_attach,
+    };
 
-	ESP_LOGI(TAG, "I have a connection and my IP is %s!", str_ip);
+    esp_http_client_handle_t client = esp_http_client_init(&config);
+    esp_http_client_set_header(client, "Content-Type", "application/json");
+    esp_http_client_set_post_field(client, request_data, strlen(request_data));
+
+    esp_err_t err = esp_http_client_perform(client);
+    if (err == ESP_OK) {
+        int http_response_code = esp_http_client_get_status_code(client);
+        ESP_LOGI("HTTP", "HTTP Code: %d", http_response_code);
+
+        char response_buffer[128];
+        int content_length = esp_http_client_get_content_length(client);
+        if (content_length > 0 && content_length < sizeof(response_buffer)) {
+            esp_http_client_read(client, response_buffer, content_length);
+            response_buffer[content_length] = '\0';
+            ESP_LOGI("HTTP", "Response: %s", response_buffer);
+        }
+
+        esp_http_client_cleanup(client);
+        return http_response_code;
+    } else {
+        ESP_LOGE("HTTP", "HTTP request failed: %s", esp_err_to_name(err));
+        esp_http_client_cleanup(client);
+        return -1;
+    }
 }
 
 void app_main(void)
@@ -52,10 +81,15 @@ void app_main(void)
 	wifi_manager_init();
 	wifi_manager_start();
 
-	/* register a callback as an example to how you can integrate your code with the wifi manager */
-	wifi_manager_set_callback(WM_EVENT_STA_GOT_IP, &cb_connection_ok);
+	while (!is_wifi_connected()) {
+        vTaskDelay(pdMS_TO_TICKS(500));
+    }
 
-	/* your code should go here. Here we simply create a task on core 2 that monitors free heap memory */
-	xTaskCreatePinnedToCore(&monitoring_task, "monitoring_task", 2048, NULL, 1, NULL, 1);
+	// TODO: If lost wifi connection per more than 10 min, start modem
+
+	if (strcmp(DEVICE_UUID, "DEFAULT"))
+	{
+		get_device_uuid(DEVICE_MAC, DEVICE_TOKEN);
+	}
 
 }
